f59ba38418e76bcf6968354427bbe31df9e62162
RTurney <r.turney93@gmail.com>
2021-02-16 14:19:27 +0000

    add second specification. 1 test fails

--- Review_1/spec/report_spec.rb
+++ Review_1/spec/report_spec.rb
@@ -5,6 +5,10 @@ require 'report'
 describe 'school report' do
 
   it "returns a string" do
-    expect(report("Green, Amber")).to be_a(String) 
+    expect(report("Green, Amber")).to be_a(String)
+  end
+
+  it "returns the number of times a specific word is within a string" do
+    expect(report("Green")).to eq "Green: 1"
   end
 end
