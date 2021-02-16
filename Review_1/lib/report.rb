9621d8aa6ba7d068918e08bf45964d98472d4669
RTurney <r.turney93@gmail.com>
2021-02-16 14:40:27 +0000

    Modified report_spec third test. 1 test failing

--- Review_1/lib/report.rb
+++ Review_1/lib/report.rb
@@ -1,7 +1,38 @@
 # Method for returning school report
 def report(teacher_report)
   word_array = teacher_report.split(', ')
+  green_num = 0
+  amber_num = 0
+  red_num = 0
+
   word_array.each do |word|
-    return (word + ": 1").to_s
+    if word == 'Green'
+      green_num += 1
+    elsif word == 'Amber'
+      amber_num += 1
+    elsif word == 'Red'
+      red_num += 1
+    end
+  end
+
+  green_report = green_checker(green_num)
+  amber_report = amber_checker(amber_num)
+  red_report = ''
+  return green_report + amber_report + red_report
+end
+
+def green_checker(green_num)
+  if green_num == 0
+    return ''
+  else
+    return 'Green: ' + green_num.to_s + "\n"
+  end
+end
+
+def amber_checker(amber_num)
+  if amber_num == 0
+    return ''
+  else
+    return "Amber: " + amber_num.to_s
   end
 end
--- Review_1/spec/report_spec.rb
+++ Review_1/spec/report_spec.rb
@@ -11,4 +11,9 @@ describe 'school report' do
   it "returns the number of times a specific word is within a string" do
     expect(report("Green")).to eq "Green: 1"
   end
+
+  it "returns the number of times a word apeeared in a sentence" do
+    expect(report("Green, Amber, Red")).to eq "Green: 1\nAmber: 1\nRed: 1"
+  end
+
 end
