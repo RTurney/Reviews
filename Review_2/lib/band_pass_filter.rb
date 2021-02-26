3abf3d87cca49e21a1973c171a9739e5b822b673
RTurney <r.turney93@gmail.com>
2021-02-26 11:21:35 +0000

    refactor band_pass_filter. All tests green

--- Review_2/lib/band_pass_filter.rb
+++ Review_2/lib/band_pass_filter.rb
@@ -1,3 +1,11 @@
 def band_pass_filter(sound_wave)
-  [40, 40, 40]
+  filtered_sound_wave = []
+  sound_wave.each do |frequency|
+    if frequency < 40
+      filtered_sound_wave.push(40)
+    else
+      filtered_sound_wave.push(frequency)
+    end
+  end
+  return filtered_sound_wave
 end
