14b0be42208ee635baec48734dc00edea1079b23
RTurney <r.turney93@gmail.com>
2021-02-26 11:23:51 +0000

    Add upperlimit to band_pass_filter. All tests green

--- Review_2/lib/band_pass_filter.rb
+++ Review_2/lib/band_pass_filter.rb
@@ -3,6 +3,8 @@ def band_pass_filter(sound_wave)
   sound_wave.each do |frequency|
     if frequency < 40
       filtered_sound_wave.push(40)
+    elsif frequency > 1000
+      filtered_sound_wave.push(1000)
     else
       filtered_sound_wave.push(frequency)
     end
--- Review_2/spec/band_pass_filter_spec.rb
+++ Review_2/spec/band_pass_filter_spec.rb
@@ -5,4 +5,8 @@ describe 'band_pass_filter' do
   it "will return [40, 40, 40] for an array of [1, 1, 1]" do
     expect(band_pass_filter([1, 1, 1])).to eq [40, 40, 40]
   end
+
+  it "will return [1000] for an array of [1001]" do
+    expect(band_pass_filter([1001])).to eq [1000]
+  end
 end
