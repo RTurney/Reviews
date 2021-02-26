21f0248b2990138a447767d4f5150c08507722fe
RTurney <r.turney93@gmail.com>
2021-02-26 11:35:00 +0000

    refactor band pass filter. All tests green

--- Review_2/lib/band_pass_filter.rb
+++ Review_2/lib/band_pass_filter.rb
@@ -1,17 +1,14 @@
 def band_pass_filter(sound_wave)
-  if sound_wave.is_a? Array
-    filtered_sound_wave = []
-    sound_wave.each do |frequency|
-      if frequency < 40
-        filtered_sound_wave.push(40)
-      elsif frequency > 1000
-        filtered_sound_wave.push(1000)
-      else
-        filtered_sound_wave.push(frequency)
-      end
+raise 'sound waves not passed correctly' unless sound_wave.is_a? Array
+  filtered_sound_wave = []
+  sound_wave.each do |frequency|
+    if frequency < 40
+      filtered_sound_wave.push(40)
+    elsif frequency > 1000
+      filtered_sound_wave.push(1000)
+    else
+      filtered_sound_wave.push(frequency)
     end
-    return filtered_sound_wave
-  else
-    raise 'sound waves not passed correctly'
   end
+  return filtered_sound_wave
 end
