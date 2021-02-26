c83c24508725959752729c5b1b3240bef859c03f
RTurney <r.turney93@gmail.com>
2021-02-26 11:33:58 +0000

    band_pass_filter throws error if passed non-array. ALl tests green

--- Review_2/lib/band_pass_filter.rb
+++ Review_2/lib/band_pass_filter.rb
@@ -1,13 +1,17 @@
 def band_pass_filter(sound_wave)
-  filtered_sound_wave = []
-  sound_wave.each do |frequency|
-    if frequency < 40
-      filtered_sound_wave.push(40)
-    elsif frequency > 1000
-      filtered_sound_wave.push(1000)
-    else
-      filtered_sound_wave.push(frequency)
+  if sound_wave.is_a? Array
+    filtered_sound_wave = []
+    sound_wave.each do |frequency|
+      if frequency < 40
+        filtered_sound_wave.push(40)
+      elsif frequency > 1000
+        filtered_sound_wave.push(1000)
+      else
+        filtered_sound_wave.push(frequency)
+      end
     end
+    return filtered_sound_wave
+  else
+    raise 'sound waves not passed correctly'
   end
-  return filtered_sound_wave
 end
--- Review_2/spec/band_pass_filter_spec.rb
+++ Review_2/spec/band_pass_filter_spec.rb
@@ -9,4 +9,16 @@ describe 'band_pass_filter' do
   it "will return [1000] for an array of [1001]" do
     expect(band_pass_filter([1001])).to eq [1000]
   end
+
+  it " will return [50] for [50]" do
+    expect(band_pass_filter([50])).to eq [50]
+  end
+
+  it "will return [60,40,45,60,1000] for [60,10,45,60,1500]" do
+    expect(band_pass_filter([60,10,45,60,1500])).to eq [60,40,45,60,1000]
+  end
+
+  it "will throw an error 'sound waves not passed correctly' if non-array passed in" do
+    expect{ band_pass_filter('string') }.to raise_error('sound waves not passed correctly')
+  end
 end
