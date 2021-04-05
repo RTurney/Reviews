f6cc01521786c6b754fae7cd96dcd2fc5198d442
RTurney <r.turney93@gmail.com>
2021-04-05 15:17:54 +0100

    Update evaluator function to assess input of '2'

--- Review_4/lib/evaluator.rb
+++ Review_4/lib/evaluator.rb
@@ -1,4 +1,8 @@
 
 def evaluator(sum)
-  [sum, 1]
+  if sum == "2"
+    [sum, 2]
+  else
+    [sum, 1]
+  end
 end
--- Review_4/spec/evaluator_spec.rb
+++ Review_4/spec/evaluator_spec.rb
@@ -5,4 +5,8 @@ describe 'evaluator' do
   it "will return ['1', 1] for 1" do
     expect(evaluator("1")).to eq ["1", 1]
   end
+
+  it "will return ['2', 2] for '2'" do
+    expect(evaluator("2")).to eq ["2", 2]
+  end
 end
